fe22d78ef1cb4b26df793f57fcd5ee08c2e59391
CharelMarais <maraischarel@gmail.com>
2023-03-28 13:22:15 +0200

    feat: :sparkles: initialised tailwind ng-zoro

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}", "index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-green-main": "#43e8c3",
+        "dark-background-main": "#111e25",
+        "font-main": "#ececfa",
+        "blue-green-secondary": "#5cddc6",
+      },
+    },
   },
   plugins: [],
-}
+};
